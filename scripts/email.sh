4c96853c8cc527d8f84339597d6f32902464891b
ksripathi <kammari.sripathi@gmail.com>
2015-09-05 10:22:58 +0530

    Create email.sh

---
+++ scripts/email.sh
@@ -0,0 +1,5 @@
+sed '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/apache2/sites-available/default
+#install sendmail
+apt-get install sendmail
+echo "define(\`SMART_HOST',\`smtp.admin.iiit.ac.in')" >> /etc/mail/sendmail.mc
+service sendmail restart
