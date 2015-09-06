8e1aa22a181b53a8ddd81e2e8197f5526b1b1fe3
ksripathi <kammari.sripathi@gmail.com>
2015-09-06 15:13:30 +0530

    Update email.sh

--- scripts/email.sh
+++ scripts/email.sh
@@ -1,4 +1,4 @@
-sed '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/apache2/sites-available/default
+sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/apache2/sites-available/default
 #install sendmail
 apt-get install sendmail
 echo "define(\`SMART_HOST',\`smtp.admin.iiit.ac.in')" >> /etc/mail/sendmail.mc
