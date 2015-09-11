f9a629db95d72c1acf9ceff639253b04d27f9a8a
sripathi <kammari.sripathi@gmail.com>
2015-09-11 17:06:35 +0530

    Added installing mysql

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Load the  config.sh using source comamnd
 source config.sh
+apt-get install expect -y
+export DEBIAN_FRONTEND=noninteractive
+#mysql server installation
+echo "Installing mysql-server"
+apt-get -q -y install mysql-server
+mysqladmin -h $HOST_NAME -u $USER_NAME password $PASSWORD
 expect db_setup.exe $HOST_NAME $USER_NAME $PASSWORD $DATABASE_NAME $PATH_FOR_SQL_FILE
  
